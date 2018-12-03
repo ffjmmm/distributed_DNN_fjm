06d9a9cfd28286c3f8f4b410ef728cfcbd0f88d0
ffjmmm <ffjmmm@outlook.com>
2018-12-03 19:31:36 +0800

    update

--- vgg_new.py
+++ vgg_new.py
@@ -65,6 +65,7 @@ class lossy_Conv2d_new(nn.Module):
 
             alpha = 0.5
 
+            '''
             x11 = F.dropout(x11, p=0.5, training=True)
             x12 = F.dropout(x12, p=0.5, training=True)
             x21 = F.dropout(x21, p=0.5, training=True)
@@ -75,7 +76,6 @@ class lossy_Conv2d_new(nn.Module):
             x21 = F.pad(x21, (1, 0, 0, 1, 0, 0, 0, 0))
             x22 = F.pad(x22, (0, 1, 0, 1, 0, 0, 0, 0))
 
-
             x11[:, :, 1: dim[2] // pieces[0] + 1, 1: dim[3] // pieces[1] + 1] = \
                 x[:, :, 0: dim[2] // pieces[0], 0: dim[3] // pieces[1]]
             x12[:, :, 1: dim[2] // pieces[0] + 1, 1: dim[3] // pieces[1] + 1] = \
@@ -84,9 +84,8 @@ class lossy_Conv2d_new(nn.Module):
                 x[:, :, dim[2] // pieces[0]: dim[2], 0: dim[3] // pieces[1]]
             x22[:, :, 1: dim[2] // pieces[0] + 1, 1: dim[3] // pieces[1] + 1] = \
                 x[:, :, dim[2] // pieces[0]: dim[2], dim[3] // pieces[1]: dim[3]]
-
-
             '''
+
             x11[:, :, dim[2] // pieces[0], 0: dim[3] // pieces[1] + 1] = \
                 F.dropout(x11[:, :, dim[2] // pieces[0], 0: dim[3] // pieces[1] + 1], p=alpha, training=True) * alpha
             x11[:, :, 0: dim[2] // pieces[0] + 1, dim[3] // pieces[1]] = \
@@ -106,7 +105,11 @@ class lossy_Conv2d_new(nn.Module):
                 F.dropout(x11[:, :, 0, 0: dim[3] // pieces[1] + 1], p=alpha, training=True) * alpha
             x22[:, :, 0: dim[2] // pieces[0] + 1, 0] = \
                 F.dropout(x11[:, :, 0: dim[2] // pieces[0] + 1, 0], p=alpha, training=True) * alpha
-            '''
+
+            x11 = F.pad(x11, (1, 0, 1, 0, 0, 0, 0, 0))
+            x12 = F.pad(x12, (0, 1, 1, 0, 0, 0, 0, 0))
+            x21 = F.pad(x21, (1, 0, 0, 1, 0, 0, 0, 0))
+            x22 = F.pad(x22, (0, 1, 0, 1, 0, 0, 0, 0))
 
             return x11.cuda(), x12.cuda(), x21.cuda(), x22.cuda()
 
