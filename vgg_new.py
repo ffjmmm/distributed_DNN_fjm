40e4caf9df906c693435b45e2561b411a0036583
ffjmmm <ffjmmm@outlook.com>
2018-12-04 19:35:03 +0800

    update

--- vgg_new.py
+++ vgg_new.py
@@ -107,17 +107,18 @@ class lossy_Conv2d_new(nn.Module):
             x21 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
             x22 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
 
+
+            x11 = x11.cuda()
+            x12 = x12.cuda()
+            x21 = x21.cuda()
+            x22 = x22.cuda()
+            
             x11.copy_(x[:, :, 0: dim[2] // pieces[0], 0: dim[3] // pieces[1]])
             x12.copy_(x[:, :, 0: dim[2] // pieces[0], dim[3] // pieces[1]: dim[3]])
             x21.copy_(x[:, :, dim[2] // pieces[0]: dim[2], 0: dim[3] // pieces[1]])
             x22.copy_(x[:, :, dim[2] // pieces[0]: dim[2], dim[3] // pieces[1]: dim[3]])
 
-            '''
-            x11 = x11.cuda()
-            x12 = x12.cuda()
-            x21 = x21.cuda()
-            x22 = x22.cuda()
-            '''
+
             '''
             x11 = x[:, :, 0: dim[2] // pieces[0] + 1, 0: dim[3] // pieces[1] + 1]
             x12 = x[:, :, 0: dim[2] // pieces[0] + 1, dim[3] - 1 // pieces[1]: dim[3]]
