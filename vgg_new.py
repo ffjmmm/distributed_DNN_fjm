a17bb5e31da8e23f1f716c1578b583eac0c1aba0
ffjmmm <ffjmmm@outlook.com>
2018-12-04 19:43:55 +0800

    update

--- vgg_new.py
+++ vgg_new.py
@@ -141,10 +141,10 @@ class lossy_Conv2d_new(nn.Module):
             x21 = F.pad(x21, (1, 1, 1, 1, 0, 0, 0, 0))
             x22 = F.pad(x22, (1, 1, 1, 1, 0, 0, 0, 0))
             '''
-            x11 = F.dropout(x11, p=0.5, training=True)
-            x12 = F.dropout(x12, p=0.5, training=True)
-            x21 = F.dropout(x21, p=0.5, training=True)
-            x22 = F.dropout(x22, p=0.5, training=True)
+            x11 = F.dropout(x11, p=0.5, training=True) * 0.5
+            x12 = F.dropout(x12, p=0.5, training=True) * 0.5
+            x21 = F.dropout(x21, p=0.5, training=True) * 0.5
+            x22 = F.dropout(x22, p=0.5, training=True) * 0.5
 
             x11[:, :, 1: dim[2] // 2 + 1, 1: dim[3] // 2 + 1] = x[:, :, 0: dim[2] // pieces[0], 0: dim[3] // pieces[1]]
             x12[:, :, 1: dim[2] // 2 + 1, 1: dim[3] // 2 + 1] = x[:, :, 0: dim[2] // pieces[0], dim[3] // pieces[1]: dim[3]]
