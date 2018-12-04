b5a0f2187b6c97a9526ea9ceb09619b0c4768e22
ffjmmm <ffjmmm@outlook.com>
2018-12-04 19:25:34 +0800

    update

--- vgg_new.py
+++ vgg_new.py
@@ -96,25 +96,25 @@ class lossy_Conv2d_new(nn.Module):
         def split(x, pieces):
             
             dim = x.shape
-            x11 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
-            x12 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
-            x21 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
-            x22 = torch.empty((dim[0], dim[1], dim[2] // pieces[0], dim[3] // pieces[1]))
+            x11 = torch.empty((dim[0], dim[1], dim[2] // pieces[0] + 1, dim[3] // pieces[1] + 1))
+            x12 = torch.empty((dim[0], dim[1], dim[2] // pieces[0] + 1, dim[3] // pieces[1] + 1))
+            x21 = torch.empty((dim[0], dim[1], dim[2] // pieces[0] + 1, dim[3] // pieces[1] + 1))
+            x22 = torch.empty((dim[0], dim[1], dim[2] // pieces[0] + 1, dim[3] // pieces[1] + 1))
             '''
             x11 = x11.cuda()
             x12 = x12.cuda()
             x21 = x21.cuda()
             x22 = x22.cuda()
             '''
-            x11 = x[:, :, 0: dim[2] // pieces[0], 0: dim[3] // pieces[1]]
-            x12 = x[:, :, 0: dim[2] // pieces[0], dim[3] // pieces[1]: dim[3]]
-            x21 = x[:, :, dim[2] // pieces[0]: dim[2], 0: dim[3] // pieces[1]]
-            x22 = x[:, :, dim[2] // pieces[0]: dim[2], dim[3] // pieces[1]: dim[3]]
+            x11 = x[:, :, 0: dim[2] // pieces[0] + 1, 0: dim[3] // pieces[1] + 1]
+            x12 = x[:, :, 0: dim[2] // pieces[0] + 1, dim[3] - 1 // pieces[1]: dim[3]]
+            x21 = x[:, :, dim[2] - 1 // pieces[0]: dim[2], 0: dim[3] // pieces[1] + 1]
+            x22 = x[:, :, dim[2] - 1 // pieces[0]: dim[2], dim[3] - 1 // pieces[1]: dim[3]]
 
-            x11 = F.pad(x11, (1, 1, 1, 1, 0, 0, 0, 0))
-            x12 = F.pad(x12, (1, 1, 1, 1, 0, 0, 0, 0))
-            x21 = F.pad(x21, (1, 1, 1, 1, 0, 0, 0, 0))
-            x22 = F.pad(x22, (1, 1, 1, 1, 0, 0, 0, 0))
+            x11 = F.pad(x11, (1, 0, 1, 0, 0, 0, 0, 0))
+            x12 = F.pad(x12, (1, 0, 0, 1, 0, 0, 0, 0))
+            x21 = F.pad(x21, (0, 1, 1, 0, 0, 0, 0, 0))
+            x22 = F.pad(x22, (0, 1, 0, 1, 0, 0, 0, 0))
 
             x11 = F.dropout(x11, p=0.5, training=True)
             x12 = F.dropout(x12, p=0.5, training=True)
