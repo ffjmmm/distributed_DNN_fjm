eed53b57e5b6f4dbbbb2cf4175941cafde33ed09
ffjmmm <ffjmmm@outlook.com>
2018-12-04 21:26:19 +0800

    update

--- vgg_new.py
+++ vgg_new.py
@@ -160,11 +160,6 @@ class lossy_Conv2d_new(nn.Module):
             x22 = x[:, :, dim[2] - 1 // pieces[0]: dim[2], dim[3] - 1 // pieces[1]: dim[3]]
             '''
 
-            x11 = F.pad(x11, (1, 0, 1, 0, 0, 0, 0, 0))
-            x12 = F.pad(x12, (0, 1, 1, 0, 0, 0, 0, 0))
-            x21 = F.pad(x21, (1, 0, 0, 1, 0, 0, 0, 0))
-            x22 = F.pad(x22, (0, 1, 0, 1, 0, 0, 0, 0))
-
             '''
             x11 = F.pad(x11, (1, 1, 1, 1, 0, 0, 0, 0))
             x12 = F.pad(x12, (1, 1, 1, 1, 0, 0, 0, 0))
@@ -177,6 +172,11 @@ class lossy_Conv2d_new(nn.Module):
             x21 = F.dropout(x21, p=alpha, training=True) * (1 - alpha)
             x22 = F.dropout(x22, p=alpha, training=True) * (1 - alpha)
 
+            x11 = F.pad(x11, (1, 0, 1, 0, 0, 0, 0, 0))
+            x12 = F.pad(x12, (0, 1, 1, 0, 0, 0, 0, 0))
+            x21 = F.pad(x21, (1, 0, 0, 1, 0, 0, 0, 0))
+            x22 = F.pad(x22, (0, 1, 0, 1, 0, 0, 0, 0))
+
             x11[:, :, 1: dim[2] // 2 + 1, 1: dim[3] // 2 + 1] = x[:, :, 0: dim[2] // pieces[0], 0: dim[3] // pieces[1]]
             x12[:, :, 1: dim[2] // 2 + 1, 1: dim[3] // 2 + 1] = x[:, :, 0: dim[2] // pieces[0], dim[3] // pieces[1]: dim[3]]
             x21[:, :, 1: dim[2] // 2 + 1, 1: dim[3] // 2 + 1] = x[:, :, dim[2] // pieces[0]: dim[2], 0: dim[3] // pieces[1]]
@@ -184,7 +184,7 @@ class lossy_Conv2d_new(nn.Module):
 
             return x11.cuda(), x12.cuda(), x21.cuda(), x22.cuda()
 
-        # x11, x12, x21, x22 = split_2(x, self.pieces)
+        x11, x12, x21, x22 = split_2(x, self.pieces)
 
         '''
         x11 = split_rand(x, self.pieces, 0, 0)
@@ -192,11 +192,12 @@ class lossy_Conv2d_new(nn.Module):
         x21 = split_rand(x, self.pieces, 1, 0)
         x22 = split_rand(x, self.pieces, 1, 1)
         '''
-
+        '''
         x11 = split_dropout(x, self.pieces, 0, 0)
         x12 = split_dropout(x, self.pieces, 0, 1)
         x21 = split_dropout(x, self.pieces, 1, 0)
         x22 = split_dropout(x, self.pieces, 1, 1)
+        '''
 
         # time1 = time.time()
         r11 = self.b1(x11)
